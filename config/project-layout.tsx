8f10cc31ba4508326e6dcec89b8d01d369ce7c2a
JosePabloSG <suarezgomezjosepablo03@gmail.com>
2024-12-23 22:46:02 -0600

    feat: update project layouts and enhance image handling for landing page and HR management

--- config/project-layout.tsx
+++ config/project-layout.tsx
@@ -2,9 +2,9 @@ import { Layout } from 'react-grid-layout';
 
 export const lgLayout: Layout[] = [
     { i: 'images-1', x: 0, y: 0, w: 2, h: 1 },
-    { i: 'images-2', x: 2, y: 0, w: 2, h: 1 },
-    { i: 'images-3', x: 3, y: 0, w: 2, h: 1 },
-    { i: 'images-4', x: 0, y: 1, w: 2, h: 1 },
+    { i: 'images-2', x: 2, y: 0, w: 1, h: 1 },
+    { i: 'images-3', x: 3, y: 0, w: 1, h: 2 },
+    { i: 'images-4', x: 0, y: 1, w: 1, h: 1 },
     { i: 'images-5', x: 1, y: 1, w: 2, h: 1 },
 ];
 
